53ca263064e55afa85b5c874aadf123efcc7f5c5
Adrian Wälchli <aedu.waelchli@gmail.com>
2020-12-22 08:45:15 +0100

    fix a few tests

--- tests/trainer/test_dataloaders.py
+++ tests/trainer/test_dataloaders.py
@@ -130,7 +130,7 @@ def test_multiple_val_dataloader(tmpdir):
 
     # make sure predictions are good for each val set
     for dataloader in trainer.val_dataloaders:
-        tpipes.run_prediction(dataloader, trainer.model)
+        tpipes.run_prediction(dataloader, model)
 
 
 @pytest.mark.parametrize('ckpt_path', [None, 'best', 'specific'])
