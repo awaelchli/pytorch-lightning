aba9eb5be419c9188680d093aee6af31e23614f4
Adrian Wälchli <aedu.waelchli@gmail.com>
2020-12-26 05:46:39 +0100

    ad

--- official.py
+++ official.py
@@ -1,6 +1,4 @@
 import os
-import sys
-import tempfile
 import torch
 import torch.distributed as dist
 import torch.nn as nn
@@ -17,8 +15,10 @@ def setup(rank, world_size):
     # initialize the process group
     dist.init_process_group("nccl", rank=rank, world_size=world_size)
 
+
 def cleanup():
     dist.destroy_process_group()
+
 
 class ToyModel(nn.Module):
 
@@ -34,18 +34,20 @@ class ToyModel(nn.Module):
 
 def demo_basic(rank, world_size):
     print(f"Running basic DDP example on rank {rank}.")
+    gpus = [1, 2]
+    root_gpu = torch.device("cuda", gpus[rank])
     setup(rank, world_size)
 
     # create model and move it to GPU with id rank
-    model = ToyModel().to(rank)
-    ddp_model = DDP(model, device_ids=[rank])
+    model = ToyModel().to(root_gpu)
+    ddp_model = DDP(model, device_ids=[gpus[rank]])
 
     loss_fn = nn.MSELoss()
     optimizer = optim.SGD(ddp_model.parameters(), lr=0.001)
 
     optimizer.zero_grad()
     outputs = ddp_model(torch.randn(20, 10))
-    labels = torch.randn(20, 5).to(rank)
+    labels = torch.randn(20, 5).to(root_gpu)
     loss_fn(outputs, labels).backward()
     optimizer.step()
 
