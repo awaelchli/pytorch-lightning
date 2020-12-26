4112131e3dab0ffa8bdda3fc3811db9672278d9a
Adrian Wälchli <aedu.waelchli@gmail.com>
2020-12-26 06:19:33 +0100

    ad

--- pytorch_lightning/accelerators/ddp_accelerator.py
+++ pytorch_lightning/accelerators/ddp_accelerator.py
@@ -262,7 +262,7 @@ class DDPAccelerator(Accelerator):
         # torch.distributed.barrier()
 
 
-        model = model.cuda(self.trainer.data_parallel_device_ids[process_idx])
+        model = model.to(torch.device(self.trainer.data_parallel_device_ids[process_idx]))
         model = DistributedDataParallel(model, device_ids=[self.trainer.data_parallel_device_ids[process_idx]])
         torch_distrib.barrier()
         # print(self.trainer.global_rank, "barrier 6")
