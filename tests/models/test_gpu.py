da886bf1d1be23ae7aec911187ab3f8f26431350
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-01-07 21:21:02 +0100

    fix import

--- tests/models/test_gpu.py
+++ tests/models/test_gpu.py
@@ -23,7 +23,7 @@ import tests.base.develop_utils as tutils
 from pytorch_lightning import Trainer
 from pytorch_lightning.utilities import device_parser
 from pytorch_lightning.utilities.exceptions import MisconfigurationException
-
+from tests.base import BoringModel
 
 PRETEND_N_OF_GPUS = 16
 
