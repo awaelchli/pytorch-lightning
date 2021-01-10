b1021982fd104efb94c2905ad75455ebce257371
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-01-10 05:25:07 +0100

    d

--- pytorch_lightning/accelerators/gpu.py
+++ pytorch_lightning/accelerators/gpu.py
@@ -7,7 +7,7 @@ class GPUAccelerator(Accelerator):
     def setup(self, trainer, model):
         if "cuda" not in str(self.root_device):
             raise MisconfigurationException(f"Device should be GPU, got {self.root_device} instead")
-        torch.cuda.set_device(self.root_device)
+        # torch.cuda.set_device(self.root_device)
         model.to(self.root_device)
 
         return super().setup(trainer, model)
